0122de2b6c75785672496048eb9bd3e43a9db6a6
李俊賢 <jmis0546@gmail.com>
2021-06-06 16:09:06 +0800

    trying to add css

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/* Icon fonts */
+import "./assets/fonts/ionicons.css";
+import "./assets/fonts/linearicons.css";
+import "./assets/fonts/open-iconic.css";
+import "./assets/fonts/pe-icon-7-stroke.css";
+import "./assets/fonts/feather.css";
+/* Core stylesheets */
+import "./assets/css/bootstrap-material.css";
+import "./assets/css/shreerang-material.css";
+import "./assets/css/uikit.css";
+/* Libs */
+import "./assets/libs/perfect-scrollbar/perfect-scrollbar.css";
+
 
 createApp(App).use(router).mount('#app')
