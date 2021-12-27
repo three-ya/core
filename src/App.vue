da0694fd853a68d95a386c2db5c5142a4b01efe3
李俊賢 <jmis0546@gmail.com>
2021-12-27 12:52:36 +0800

    delete admin.vue

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,3 @@ export default {
   name: 'App',
 }
 </script>
-
-<style>
-@import "./assets/fonts/fontawesome.scss";
-@import "./assets/fonts/ionicons.scss";
-@import "./assets/fonts/linearicons.scss";
-@import "./assets/fonts/open-iconic.scss";
-@import "./assets/fonts/pe-icon-7-stroke.scss";
-@import "./assets/fonts/feather.scss";
-/* Core stylesheets */
-@import "./assets/scss/bootstrap-material.scss";
-@import "./assets/scss/shreerang-material.scss";
-@import "./assets/scss/uikit.scss";
-/* Libs */
-@import "./assets/libs/perfect-scrollbar/perfect-scrollbar.scss";
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/admin',
-    name: 'Admin',
-    // route level code-splitting
-    // this generates a separate chunk (admin.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "admin" */ '../views/Admin.vue')
   }
 ]
 
